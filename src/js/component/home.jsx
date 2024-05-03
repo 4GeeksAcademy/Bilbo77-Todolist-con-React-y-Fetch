20df821431a22510498beb821c48f9562d10044b
Bilbo L.R <bilbo.lr@gmail.com>
2024-05-03 16:02:02 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,11 @@
 import React from "react";
+import { Bilbolist_viva_Edu } from "./Bilbolist_viva_Edu.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		   <Bilbolist_viva_Edu />
+		</>
 	);
 };
 
